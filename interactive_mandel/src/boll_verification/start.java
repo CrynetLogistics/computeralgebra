83fcff985e1bab2c165fa205e5fe480f70a1790b
Zichen Liu <zichen.liu14@imperial.ac.uk>
2015-03-17 17:43:18 +0000

    M5 (Cleanup)

--- interactive_mandel/src/boll_verification/start.java
+++ interactive_mandel/src/boll_verification/start.java
@@ -6,30 +6,35 @@ public class start {
 	private static double THRESHOLD_TO_TAKE_AS_INFINITY = 2;
 	
 	public static void main(String[] args) {
-		///////////////////////////////////////////////DO THIS FOR CUBES AND HIGHER AND LOWER POWERS AND COMPLEX POWERS
-		for(int i=0;i<20;i++){
-			double epsilon = Math.pow(2, -1*i);
+		/////////////////////////////////////////////DO THIS FOR CUBES AND HIGHER AND LOWER POWERS AND COMPLEX POWERS
+		for(int i=0;i<15;i++){
+			double epsilon = Math.pow(3, -1*i);
 			System.out.println(epsilon*isConvergentWithin(-0.75,epsilon,1000000000));
+			System.out.println("-");
 		}
 		System.out.println("___________________________________________________________");
-		for(int i=0;i<20;i++){
-			double epsilon = Math.pow(2, -1*i);
+		for(int i=0;i<15;i++){
+			double epsilon = Math.pow(3, -1*i);
 			System.out.println(Math.sqrt(epsilon)*isConvergentWithin(0.25+epsilon,0,1000000000));
+			System.out.println("-");
 		}
 		System.out.println("___________________________________________________________");
-		for(int i=0;i<20;i++){
-			double epsilon = Math.pow(2, -1*i);
+		for(int i=0;i<15;i++){
+			double epsilon = Math.pow(3, -1*i);
 			System.out.println(epsilon*isConvergentWithin(-0.75,-1*epsilon,1000000000));
+			System.out.println("-");
 		}
 		System.out.println("___________________________________________________________");
 		for(int i=0;i<15;i++){
 			double epsilon = Math.pow(3, -1*i);
 			System.out.println(epsilon*isConvergentWithin(-1.25,-1*epsilon,1000000000));
+			System.out.println("-");
 		}
 		System.out.println("___________________________________________________________");
 		for(int i=0;i<15;i++){
 			double epsilon = Math.pow(3, -1*i);
 			System.out.println(epsilon*isConvergentWithin(-1.25,epsilon,1000000000));
+			System.out.println("-");
 		}
 	}
 	
@@ -63,4 +68,5 @@ public class start {
 	public static double reSquareC(double a,double b){
 		return a*a-b*b;
 	}
+
 }
--- interactive_mandel/src/pixel/MandelPowerPixel.java
+++ interactive_mandel/src/pixel/MandelPowerPixel.java
@@ -8,6 +8,8 @@ public class MandelPowerPixel extends ScreenPixel {
 	double yPos;
 
 	private double THRESHOLD_TO_TAKE_AS_INFINITY = 2;
+	private double POWER_REAL_PART = 2;
+	private double POWER_IMAG_PART = -1;
 
 	private int numberOfIterations;
 	
@@ -21,7 +23,7 @@ public class MandelPowerPixel extends ScreenPixel {
 		double x=a,y=b,xt,yt;
 		ComplexPower cp;
 		for(int i=0;i<iterations;i++){
-			cp = new ComplexPower(x,y,0,2);
+			cp = new ComplexPower(x,y,POWER_REAL_PART,POWER_IMAG_PART);
 			xt = cp.realPower() + a;
 			yt = cp.imagPower() + b;
 
